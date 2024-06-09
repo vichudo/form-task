c57a8fd24d0d761cf05f428cfb1786af19e2ad13
vichudo <vichudo@gmail.com>
2024-06-09 01:21:21 -0400

    create a router to deliver excel file

--- src/server/api/routers/form.ts
+++ src/server/api/routers/form.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import ExcelJS from 'exceljs';
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const formRouter = createTRPCRouter({
@@ -69,6 +70,59 @@ export const formRouter = createTRPCRouter({
                 userId: ctx.session.user.id
             }
         })
+    }),
+    exportContactsToExcel: protectedProcedure.mutation(async ({ ctx }) => {
+        // TODO: Include in a smart way padron data in the excel
+        const contacts = await ctx.prisma.formData.findMany({
+            where: {
+                userId: ctx.session.user.id
+            }
+        });
+
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet('Contacts');
+
+        worksheet.columns = [
+            { header: 'RUT', key: 'rut', width: 20 },
+            { header: 'Nombre Completo', key: 'nombre_completo', width: 30 },
+            { header: 'Teléfono', key: 'telefono', width: 20 },
+            { header: 'Dirección', key: 'direccion', width: 30 },
+            { header: 'Comuna', key: 'comuna', width: 20 },
+            { header: 'Región', key: 'region', width: 20 },
+            { header: 'Nacionalidad', key: 'nacionalidad', width: 20 },
+            { header: 'Email', key: 'mail', width: 30 },
+            { header: 'Instagram', key: 'instagram', width: 20 },
+            { header: 'Facebook', key: 'facebook', width: 20 },
+            { header: 'Twitter', key: 'twitter', width: 20 },
+            { header: 'Etiqueta 1', key: 'etiqueta_1', width: 20 },
+            { header: 'Etiqueta 2', key: 'etiqueta_2', width: 20 },
+            { header: 'Etiqueta 3', key: 'etiqueta_3', width: 20 },
+            { header: 'Comentario', key: 'comentario', width: 30 },
+        ];
+
+        contacts.forEach(contact => {
+            worksheet.addRow({
+                rut: contact.rut,
+                nombre_completo: contact.nombre_completo,
+                telefono: contact.telefono,
+                direccion: contact.direccion,
+                comuna: contact.comuna,
+                region: contact.region,
+                nacionalidad: contact.nacionalidad,
+                mail: contact.mail,
+                instagram: contact.instagram,
+                facebook: contact.facebook,
+                twitter: contact.twitter,
+                etiqueta_1: contact.etiqueta_1,
+                etiqueta_2: contact.etiqueta_2,
+                etiqueta_3: contact.etiqueta_3,
+                comentario: contact.comentario,
+            });
+        });
+
+        const buffer = await workbook.xlsx.writeBuffer();
+        const base64String = Buffer.from(buffer).toString('base64');
+        return { base64String };
     }),
     deleteContactById: protectedProcedure
         .input(z.object({ id: z.string() }))
