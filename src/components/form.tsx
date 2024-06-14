92dc6b196189fe1824b1be99eb5ddb6898c5de07
vichudo <vichudo@gmail.com>
2024-06-14 02:40:28 -0400

    update main form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,11 +1,13 @@
 import { PadronData } from "@prisma/client";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm, Controller, FieldValues } from "react-hook-form";
 import toast from "react-hot-toast";
 import Select, { SingleValue } from "react-select";
 import { trpc } from "~/utils/api";
 import { RouterInputs } from "~/utils/api";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useRutFormatter } from "~/hooks/useFormatRut";
 
 type FormData = RouterInputs['formRouter']['submitForm'];
 
@@ -35,34 +37,50 @@ export const Form = () => {
         }
     });
 
-    const { data: session } = useSession()
+    const { data: session } = useSession();
 
-
-    const [selectedRut, setSelectedRut] = useState("");
+    const [selectedRut, setSelectedRut] = useState<string>("");
     const [selectedOption, setSelectedOption] = useState<SelectOption | null>(null);
+    const rut = watch("rut");
+    const formattedRut = useRutFormatter(String(rut));
 
-    const { data: padronData, isLoading } = trpc.search.searchByRut.useQuery(
+    useEffect(() => {
+        setValue("rut", formattedRut);
+    }, [formattedRut, setValue]);
+
+    const { data: padronData, isLoading } = trpc.search.searchByRutStrict.useQuery(
         { rut: selectedRut },
         { enabled: selectedRut.length > 0 }
     );
+
+    useEffect(() => {
+        if (padronData && padronData.length > 0) {
+            // Automatically select the first result
+            const firstOption = {
+                value: 0,
+                label: `${padronData[0]?.NOMBRES ?? ''} ${padronData[0]?.APELLIDO_PATERNO ?? ''} ${padronData[0]?.APELLIDO_MATERNO ?? ''}`
+            };
+            setSelectedOption(firstOption);
+            handlePadronDataSelect(firstOption);
+        }
+    }, [padronData]);
 
     const { mutateAsync: submitFormMutation, status } = trpc.formRouter.submitForm.useMutation();
 
     const onSubmit = async (data: FormData) => {
         try {
-            await submitFormMutation({ ...data, userId: session?.user?.id });
+            await submitFormMutation({ ...data, userId: session?.user?.id, padronDataId: padronData?.[0]?.id });
             console.log("Form submitted successfully");
-            // Reset form or perform any other actions
             reset();
-            toast.success("Contacto Agregado")
+            toast.success("Contacto Agregado");
         } catch (error) {
             console.error("Error submitting form:", error);
         }
     };
 
-    const handleRutChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const rut = e.target.value;
-        setSelectedRut(rut);
+    const handleSearchClick = () => {
+        const rut = watch("rut");
+        setSelectedRut(String(rut));
     };
 
     const handlePadronDataSelect = (selectedOption: SingleValue<SelectOption>) => {
@@ -71,21 +89,44 @@ export const Form = () => {
         const data = padronData ? padronData[selectedOption.value] : null;
         if (data) {
             setValue("rut", `${data.RUN}-${data.DV}`);
-            setValue("nombre_completo", `${data.NOMBRES} ${data.APELLIDO_PATERNO} ${data.APELLIDO_MATERNO}`);
-            setValue("direccion", `${data.CALLE} ${data.NUMERO} ${data.LETRA} ${data.RESTO_DOMICILIO}`);
+            setValue("nombre_completo", `${data.NOMBRES ?? ''} ${data.APELLIDO_PATERNO ?? ''} ${data.APELLIDO_MATERNO ?? ''}`);
+            setValue("direccion", `${data.CALLE ?? ''} ${data.NUMERO ?? ''} ${data.LETRA ?? ''} ${data.RESTO_DOMICILIO ?? ''}`);
             setValue("comuna", String(data.GLOSACOMUNA));
             setValue("region", String(data.GLOSAREGION));
             setValue("nacionalidad", String(data.GLOSAPAIS));
         }
     };
 
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            handleSearchClick();
+        }
+    };
+
+    const handleReset = () => {
+        reset();
+        setSelectedOption(null);
+        setSelectedRut("");
+    };
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
-            <div>
-                <h2 className="text-2xl font-semibold leading-7 text-gray-900">Ingreso de contacto</h2>
-                <p className="mt-1 text-sm leading-6 text-gray-600">
-                    Ingresa el rut del contacto para verificar su información del padrón, si existe un registro la información se cargará automáticamente.
-                </p>
+
+            <div className="flex justify-between items-center gap-8">
+                <div>
+                    <h2 className="text-2xl font-semibold leading-7 text-gray-900">Ingreso de contacto</h2>
+                    <p className="mt-1 text-xs md:text-sm leading-6 text-gray-600 ">
+                        Ingresa el rut del contacto para verificar su información del padrón, si existe un registro la información se cargará automáticamente.
+                    </p>
+                </div>
+                <button
+                    type="button"
+                    onClick={handleReset}
+                    className="inline-flex justify-center rounded-md bg-gray-200 px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm transition duration-150 ease-in-out hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-400"
+                >
+                    Limpiar
+                </button>
             </div>
 
             <div className="space-y-6">
@@ -93,14 +134,21 @@ export const Form = () => {
                     <label htmlFor="rut" className="block text-sm font-medium leading-6 text-gray-900">
                         RUT
                     </label>
-                    <div className="mt-2">
+                    <div className="mt-2 flex items-center space-x-2">
                         <input
                             type="text"
                             id="rut"
                             {...register("rut")}
-                            onChange={handleRutChange}
                             className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            onKeyDown={handleKeyDown}
                         />
+                        <button
+                            type="button"
+                            onClick={handleSearchClick}
+                            className="inline-flex items-center justify-center rounded-md bg-indigo-600 p-2 text-white shadow-sm transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+                        >
+                            <MagnifyingGlassIcon className="h-5 w-5" />
+                        </button>
                     </div>
                 </div>
 
@@ -139,7 +187,6 @@ export const Form = () => {
                 )}
 
                 <div>
-                    {/* Display the selected padron data here */}
                     {selectedOption && padronData && (
                         <div className="border rounded p-4 mt-4 bg-gray-100 shadow-sm">
                             <h3 className="font-bold mb-2 text-lg text-indigo-700">Datos Electorales</h3>
@@ -259,12 +306,6 @@ export const Form = () => {
             </div>
 
             <div className="mt-6 flex items-center justify-end gap-x-6">
-                <button
-                    type="button"
-                    className="text-sm font-semibold leading-6 text-gray-900 transition duration-150 ease-in-out hover:text-gray-700"
-                >
-                    Cancelar
-                </button>
                 <button
                     type="submit"
                     className="inline-flex justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-sm transition duration-150 ease-in-out hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
