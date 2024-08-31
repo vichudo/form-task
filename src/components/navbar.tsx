8224008782baa413005679dc4e864b732c11f075
vichudo <vichudo@gmail.com>
2024-08-31 13:50:25 -0400

    feat: update navbar to add messaging page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,6 @@ import { signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import socialTazkLogo from '../../public/socialTazkLogo.jpeg'
 
-
 const navigation = [
     { name: 'Contactos', href: '/app' },
     { name: 'Formulario', href: '/form' },
@@ -19,23 +18,17 @@ export const Navbar: FC = () => {
     const { data: session } = useSession()
     const router = useRouter()
 
+    // Create a new array with the Admin tab if the user is an admin
+    const navigationItems = session?.user?.subscriptionPlan === 'admin'
+        ? [...navigation, ...[{ name: 'Admin', href: '/admin' }, { name: 'Mensajes', href: '/admin/mensajes' }]]
+        : navigation
+
     return (
         <Popover as="header" className="relative">
             <div className="bg-gray-100 py-6">
                 <nav
                     className="relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6"
                     aria-label="Global">
-                    {/* <Link href={'/'} className="flex items-center drop-shadow-lg">
-                        <Image
-                            height={100}
-                            width={100}
-                            className="h-8 w-auto sm:h-10 mr-4"
-                            src={payscriptLogoImage}
-                            alt="Reccupera logo"
-                        />
-                        <span className="font-bold text-indigo-600 ">Reccupera</span>
-                    </Link> */}
-
                     <div className="flex items-center">
                         <div className="flex w-full items-center justify-between md:w-auto">
                             <div className="-mr-2 flex items-center md:hidden">
@@ -49,7 +42,7 @@ export const Navbar: FC = () => {
                             </div>
                         </div>
                         <div className="hidden space-x-8 md:ml-10 md:flex">
-                            {navigation.map(item => (
+                            {navigationItems.map(item => (
                                 <Link
                                     key={item.name}
                                     href={item.href}
@@ -62,11 +55,6 @@ export const Navbar: FC = () => {
 
                     {session ? (
                         <div className="hidden md:flex md:items-center md:space-x-8">
-                            {/* <Link
-                                href="/app/dashboard"
-                                className="block rounded-md bg-indigo-600 py-2 px-3 text-center  font-medium text-white shadow hover:bg-indigo-700">
-                                Ir a App
-                            </Link> */}
                             <button
                                 onClick={() => signOut().then(() => router.push('/login'))}
                                 className="block items-center rounded-md border border-transparent font-medium  bg-gray-200 px-4 py-2 text-base text-red-500 hover:bg-red-100">
@@ -104,13 +92,7 @@ export const Navbar: FC = () => {
                     <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
                         <div className="flex items-center justify-between px-5 pt-4">
                             <div>
-                                {/* <Image
-                                    height={100}
-                                    width={100}
-                                    className="h-10 w-auto"
-                                    src={payscriptLogoImage}
-                                    alt="Reccupera logo"
-                                /> */}
+                                {/* Logo can be added here if needed */}
                             </div>
                             <div className="-mr-2">
                                 <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
@@ -121,7 +103,7 @@ export const Navbar: FC = () => {
                         </div>
                         <div className="pt-5 pb-6">
                             <div className="space-y-1 px-2">
-                                {navigation.map(item => (
+                                {navigationItems.map(item => (
                                     <Link
                                         key={item.name}
                                         href={item.href}
@@ -130,21 +112,6 @@ export const Navbar: FC = () => {
                                     </Link>
                                 ))}
                             </div>
-                            {/* <div className="mt-6 px-5">
-                                {session ? (
-                                    <Link
-                                        href="/app/dashboard"
-                                        className="block w-full rounded-md bg-indigo-600 py-3 px-4 text-center font-medium text-white shadow hover:bg-indigo-700">
-                                        Dashboard
-                                    </Link>
-                                ) : (
-                                    <Link
-                                        href="/login"
-                                        className="block w-full rounded-md bg-indigo-600 py-3 px-4 text-center font-medium text-white shadow hover:bg-indigo-700">
-                                        Empieza
-                                    </Link>
-                                )}
-                            </div> */}
                             <div className="mt-6 px-5">
                                 {session ? (
                                     <p className="text-center text-base font-medium text-gray-500">
